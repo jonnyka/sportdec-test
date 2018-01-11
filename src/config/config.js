e5e163c18126307b671c6e098497b1242ee67d47
jonny <jonny@frogz.hu>
2018-01-11 17:02:32 +0000

    removed config from the repo

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    githubToken: '6b7b990835125cb4e482bf0144951a910228e0c2',
-    twitterConsumerKey: 'CuERb8NsmWJ4WjIdtjlmC8Yh3',
-    twitterConsumerSecret: 'JyaHGyrmZ2DYigEojWp8L0z5jElQOetPPCbSPUrnu16sN6CrHj',
-    twitterAccessTokenKey: '406634490-3kjMwvMyJ6waiF9tDsGZGKOnMBPMd19GT3IXV4VU',
-    twitterAccessTokenSecret: 'mH1CxXh1YBaPmjjkHeSGbgBVtL7r6bJ5qT1hm4EMnCyAk'
-};
